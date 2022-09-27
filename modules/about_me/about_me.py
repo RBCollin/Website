c5c0ac3339a789cb8dd60ba2029a3693d8e6e770
Bernard Collin <bernard.collin@hotmail.com>
2022-09-27 11:08:29 -0300

    Update about_me.py

--- modules/about_me/about_me.py
+++ modules/about_me/about_me.py
@@ -160,7 +160,8 @@ def about_me():
 
     if selected == 'Gmail':
         st.write('bernardcollin92@gmail.com')
-       
+        
+    from bokeh.models.widgets import Div
     if st.button('Go to Streamlit'):
         js = "window.open('https://wa.me/5575982381806')"  # New tab or window
         #js = "window.location.href = 'https://www.streamlit.io/'"  # Current tab
