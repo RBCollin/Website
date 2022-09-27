ebd4e59b1929a243825a42a1757381aeb2fd0d22
Bernard Collin <bernard.collin@hotmail.com>
2022-09-27 13:28:06 -0300

    Update cluster.py

--- modules/machine_topics/cluster.py
+++ modules/machine_topics/cluster.py
@@ -14,7 +14,7 @@ def clusters():
 
     col1.success('# Clustering')
     
-    col1.write(""" - #### In this section I will show the main strategies that I generally use in unsupervised problems. For clustering I really like to use K-Means, evaluating the number of clusters through the metric "Silhoutte Score" as well as the graphic representation of the clusters characterized by this metric. Finally, I use Plotly for 2d or 3d plots when possible to have a dimensional view of the variable groups.""")
+    col1.write(""" - #### In this section I’ll show the main strategies that generally I use to unsupervised problems. For clustering I really like to use K-Means, evaluating the number of clusters through the metric "Silhoutte Score" as well as the graphic representation of the clusters characterized by this metric. Finally, I use Plotly for 2d or 3d plots when possible to have a dimensional view of the variable groups.""")
     col1.write(' - #### To achieve these results, K-Means was used as a strategy, determining the number of clusters through the Silhoutte Score metric and the graphic performance of the groups.')
 
     col1.write('___')
@@ -39,7 +39,7 @@ def clusters():
     col3.image(img, use_column_width=True)
 
     col1.write(""" 
-    - #### The choice of 4 clusters was due to the fact that it has an acceptable number of clusters with an acceptable Silhoutte Score, it can be seen from the graphs that the amount of K=4 allows all groups to have a proportional size, the groups are above the Silhoutte Score line and with few residues present in the clusters (negative X axis).
+    - #### The choice of 4 clusters was due to the fact that it has an acceptable number of clusters and a good Silhoutte Score value, it can be seen from the graphs that the amount of K=4 allows all groups to have a proportional size, the groups are above the Silhoutte Score line and with few residues present in the clusters (negative X axis).
         - #### It is worth mentioning that the data were previously normalized and standardized to not bias the clustering and affect the centroids of the groups.
     
         """)
