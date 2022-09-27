cf688389ba0aa7466b822a2acab6991c314ec81c
Bernard Collin <bernard.collin@hotmail.com>
2022-09-27 10:43:59 -0300

    Update about_me.py

--- modules/about_me/about_me.py
+++ modules/about_me/about_me.py
@@ -148,7 +148,9 @@ def about_me():
     )
 
     if selected == 'CV-Europass':
-        webbrowser.open_new_tab('https://europa.eu/europass/eportfolio/api/eprofile/shared-profile/4bda4bc4-878d-4476-93c7-f5d5400d662a?view=html')
+        def open():
+            webbrowser.open_new_tab('https://europa.eu/europass/eportfolio/api/eprofile/shared-profile/4bda4bc4-878d-4476-93c7-f5d5400d662a?view=html')
+        open()
  
 
     if selected == 'Linkedin':
