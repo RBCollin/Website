40f94bb8f4f6a8d5638aee9bf9424dab23418f88
Bernard Collin <bernard.collin@hotmail.com>
2022-09-27 14:02:00 -0300

    Update about_me.py

--- modules/about_me/about_me.py
+++ modules/about_me/about_me.py
@@ -132,7 +132,7 @@ def about_me():
             st.image(img, use_column_width=True)
             st.empty()
 
-    coluna1, coluna2, coluna3 = st.columns([0.2,1,0.2])
+    coluna1, coluna2, coluna3 = st.columns([0.1,1,0.1])
     with coluna2:
             
         selected = option_menu(
