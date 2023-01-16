05111c59a5d33ef8dd15bdf1e1144df7163ad429
Bernard Collin <bernard.collin@hotmail.com>
2023-01-16 00:41:06 -0300

    Update about_me.py

--- modules/about_me/about_me.py
+++ modules/about_me/about_me.py
@@ -104,7 +104,7 @@ def about_me():
                 return None
             return r.json()
         st.write(' ')
-        lottie_icon = get_lottie('https://assets4.lottiefiles.com/packages/lf20_ndqyrqfd.json')
+        lottie_icon = get_lottie('https://assets3.lottiefiles.com/private_files/lf30_ps1145pz.json')
         st_lottie(lottie_icon, height = 450, key = 'lttie')
         
     with st.container():
